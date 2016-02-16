ac226ba5c2d3a4361618024772c895ff0c950949
siktirmirza <alicowman79@gmail.com>
2016-02-16 16:12:02 +0330

    Update boobs.lua

--- plugins/boobs.lua
+++ plugins/boobs.lua
@@ -40,11 +40,11 @@ end
 local function run(msg, matches)
   local url = nil
   
-  if matches[1] == "!mame" then
+  if matches[1] == "!boobs" then
     url = getRandomBoobs()
   end
 
-  if matches[1] == "!kos" then
+  if matches[1] == "!butts" then
     url = getRandomButts()
   end
 
@@ -59,12 +59,12 @@ end
 return {
   description = "Gets a random boobs or butts pic", 
   usage = {
-    "!mame: Get a boobs NSFW image. 🔞",
-    "!kos: Get a butts NSFW image. 🔞"
+    "!boobs: Get a boobs NSFW image. 🔞",
+    "!butts: Get a butts NSFW image. 🔞"
   },
   patterns = {
-    "^!mame$",
-    "^!kos$"
+    "^!boobs$",
+    "^!butts$"
   }, 
   run = run 
 }
