78a2cfdb11b49f4b3e5e46d3b862a646c4769d38
siktirmirza <alicowman79@gmail.com>
2016-02-19 14:58:00 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -1,5 +1,184 @@
 
-    "weather",
+package.path = package.path .. ';.luarocks/share/lua/5.2/?.lua'
+..';.luarocks/share/lua/5.2/?/init.lua'
+package.cpath = package.cpath .. ';.luarocks/lib/lua/5.2/?.so'
+require("./bot/utils")
+VERSION = '2'
+-- This function is called when tg receive a msg
+function on_msg_receive (msg)
+if not started then
+return
+end
+local receiver = get_receiver(msg)
+print (receiver)
+--vardump(msg)
+msg = pre_process_service_msg(msg)
+if msg_valid(msg) then
+msg = pre_process_msg(msg)
+if msg then
+match_plugins(msg)
+if redis:get("bot:markread") then
+if redis:get("bot:markread") == "on" then
+mark_read(receiver, ok_cb, false)
+end
+end
+end
+end
+end
+function ok_cb(extra, success, result)
+end
+function on_binlog_replay_end()
+started = true
+postpone (cron_plugins, false, 60*5.0)
+_config = load_config()
+-- load plugins
+plugins = {}
+load_plugins()
+end
+function msg_valid(msg)
+-- Don't process outgoing messages
+if msg.out then
+print('\27[36mNot valid: msg from us\27[39m')
+return false
+end
+-- Before bot was started
+if msg.date < now then
+print('\27[36mNot valid: old msg\27[39m')
+return false
+end
+if msg.unread == 0 then
+print('\27[36mNot valid: readed\27[39m')
+return false
+end
+if not msg.to.id then
+print('\27[36mNot valid: To id not provided\27[39m')
+return false
+end
+if not msg.from.id then
+print('\27[36mNot valid: From id not provided\27[39m')
+return false
+end
+if msg.from.id == our_id then
+print('\27[36mNot valid: Msg from our id\27[39m')
+return false
+end
+if msg.to.type == 'encr_chat' then
+print('\27[36mNot valid: Encrypted chat\27[39m')
+return false
+end
+if msg.from.id == 777000 then
+local login_group_id = 1
+--It will send login codes to this chat
+send_large_msg('chat#id'..login_group_id, msg.text)
+end
+return true
+end
+--
+function pre_process_service_msg(msg)
+if msg.service then
+local action = msg.action or {type=""}
+-- Double ! to discriminate of normal actions
+msg.text = "!!tgservice " .. action.type
+-- wipe the data to allow the bot to read service messages
+if msg.out then
+msg.out = false
+end
+if msg.from.id == our_id then
+msg.from.id = 0
+end
+end
+return msg
+end
+-- Apply plugin.pre_process function
+function pre_process_msg(msg)
+for name,plugin in pairs(plugins) do
+if plugin.pre_process and msg then
+print('Preprocess', name)
+msg = plugin.pre_process(msg)
+end
+end
+return msg
+end
+-- Go over enabled plugins patterns.
+function match_plugins(msg)
+for name, plugin in pairs(plugins) do
+match_plugin(plugin, name, msg)
+end
+end
+-- Check if plugin is on _config.disabled_plugin_on_chat table
+local function is_plugin_disabled_on_chat(plugin_name, receiver)
+local disabled_chats = _config.disabled_plugin_on_chat
+-- Table exists and chat has disabled plugins
+if disabled_chats and disabled_chats[receiver] then
+-- Checks if plugin is disabled on this chat
+for disabled_plugin,disabled in pairs(disabled_chats[receiver]) do
+if disabled_plugin == plugin_name and disabled then
+local warning = 'Plugin '..disabled_plugin..' is disabled on this chat'
+print(warning)
+send_msg(receiver, warning, ok_cb, false)
+return true
+end
+end
+end
+return false
+end
+function match_plugin(plugin, plugin_name, msg)
+local receiver = get_receiver(msg)
+-- Go over patterns. If one matches it's enough.
+for k, pattern in pairs(plugin.patterns) do
+local matches = match_pattern(pattern, msg.text)
+if matches then
+print("msg matches: ", pattern)
+if is_plugin_disabled_on_chat(plugin_name, receiver) then
+return nil
+end
+-- Function exists
+if plugin.run then
+-- If plugin is for privileged users only
+if not warns_user_not_allowed(plugin, msg) then
+local result = plugin.run(msg, matches)
+if result then
+send_large_msg(receiver, result)
+end
+end
+end
+-- One patterns matches
+return
+end
+end
+end
+-- DEPRECATED, use send_large_msg(destination, text)
+function _send_msg(destination, text)
+send_large_msg(destination, text)
+end
+-- Save the content of _config to config.lua
+function save_config( )
+serialize_to_file(_config, './data/config.lua')
+print ('saved config into ./data/config.lua')
+end
+-- Returns the config from config.lua file.
+-- If file doesn't exist, create it.
+function load_config( )
+local f = io.open('./data/config.lua', "r")
+-- If config.lua doesn't exist
+if not f then
+print ("Created new config file: data/config.lua")
+create_config()
+else
+f:close()
+end
+local config = loadfile ("./data/config.lua")()
+for v,user in pairs(config.sudo_users) do
+print("Allowed user: " .. user)
+end
+return config
+end
+-- Create a basic config.json file and saves it.
+function create_config( )
+-- A simple config with basic plugins and ourselves as privileged user
+config = {
+enabled_plugins = {
+ "weather",
     "goftar",
     "plugins",
     "boobs",
@@ -20,10 +199,10 @@
     "all",
     "leave_ban",
     "admin"
-    },
-    sudo_users = {25866548},--Sudo users
-    disabled_channels = {},
-    moderation = {data = 'data/moderation.json'},
+},
+sudo_users = {25866548},--Sudo user
+disabled_channels = {},
+moderation = {data = 'data/moderation.json'},
     about_text = [[ Paradise Bot V 8.5
 Coding By SikkTir Mirza and Ali.Destroyer
 @ShopBuy
@@ -34,268 +213,176 @@ Matin Shadow
 mohammad tictic
 All Right Reserved !
 ]],
-    help_text_realm = [[
-کد های گپ اصلی
-
+help_text_realm = [[
+Realm Commands:
 !creategroup [Name]
-ساخت گروه
-
+Create a group
 !createrealm [Name]
-ساخت گپ اصلی
-
+Create a realm
 !setname [Name]
-تنظیم اسم گپ اصلی
-
+Set realm name
 !setabout [GroupID] [Text]
-تنظیم متن درباره
-
+Set a group's about text
 !setrules [GroupID] [Text]
-تنظیم قوانین گروه
-
+Set a group's rules
 !lock [GroupID] [setting]
-قفل تنظیمات گروه
-
+Lock a group's setting
 !unlock [GroupID] [setting]
-بازکردن تنظیمات گروه
-
+Unock a group's setting
 !wholist
-لیست اعضا
-
+Get a list of members in group/realm
 !who
-ارسال لیست اعضا در یک فایل
-
+Get a file of members in group/realm
 !type
-تنظیم مدل گروه
-
+Get group type
 !kill chat [GroupID]
-حذف همه اعضای گروه
-
+Kick all memebers and delete group
 !kill realm [RealmID]
-حذف همه ی اعضای گپ اصلی
-
+Kick all members and delete realm
 !addadmin [id|username]
-اضافه کردن مدیر (فقط مالک
-
+Promote an admin by id OR username *Sudo only
 !removeadmin [id|username]
-حذف مدیر *فقط مالک
-
+Demote an admin by id OR username *Sudo only
 !list groups
-لیست گروه ها
-
+Get a list of all groups
 !list realms
-لیست گپ های اصلی
-
+Get a list of all realms
 !log
-دریافت لیست چت های گروه حاضر
-
-بگو [text]
-بگو Hello !
-ارسال پیام شما به همه
-فقط مالک این کد را می تواند استفاده کند
-
+Grt a logfile of current group or realm
+!broadcast [text]
+!broadcast Hello !
+Send text to all groups
+Only sudo users can run this command
 !bc [group_id] [text]
 !bc 123456789 Hello !
-پیام مورد نظر شما به گروه میرود [group_id]
-
-
-شما می توانید دستورات را هم با ! و هم با / بزنید
-
-
-فقط مدیران و مالک می توانند ربات در گروه های قفل بیاورند
-
-
-فقط مقام های بالاتر از کمکی میتوانند از دستورات زیر استفاده کنند
- kick,ban,unban,newlink,setphoto,setname,lock,unlock,set rules,set about and settings commands
-
-فقط سرگروه به بالا میتواند دستورات زیر را اجرا کند
- res, setowner, commands
+This command will send text to [group_id]
 ]],
-    help_text = [[
+help_text = [[
 Commands list :
-
 !kick [username|id]
-می توانید با ریپلی هم کیک کنید
-
+You can also do it by reply
 !ban [ username|id]
-با ریپلی هم میشود
-
+You can also do it by reply
 !unban [id]
-با ریپلی هم میشود
+You can also do it by reply
 !who
-لیست اعضا
-
+Members list
 !modlist
-لیست کمکی ها
-
+Moderators list
 !promote [username]
-کمکی کردن کسی
-
+Promote someone
 !demote [username]
-خلع مقام کردن
-
-خروج از گروه
-
+Demote someone
+!kickme
+Will kick user
 !about
-درباره ی گروه
-
+Group description
 !setphoto
-تنظیم عکس گروه
-
+Set and locks group photo
 !setname [name]
-تنظیم اسم گروه
-
+Set group name
 !rules
-قوانین گروه
-
+Group rules
 !id
-فهمیدن ایدی گروه یا فردی با ریپلی کردن
-
+return group id or user id
 !help
-همین پیام
-
-!lock [member|name|bots|leave]	
-قفل کردن [اعضا|اسمbربات هاlخروج 
-
+!lock [member|name|bots|leave]
+Locks [member|name|bots|leaveing]
 !unlock [member|name|bots|leave]
-بازکردن موارد قفل شده
-
+Unlocks [member|name|bots|leaving]
 !set rules <text>
-تنظیم قوانین
-
+Set <text> as rules
 !set about <text>
-تنظیم متن درباره
-
+Set <text> as about
 !settings
-تنظیمات
-
+Returns group settings
 !newlink
-لینک جدید
-
+create/revoke your group link
 !link
-ارسال لینک
-
+returns group link
 !owner
-فهمیدن سرگروه
-
+returns group owner id
 !setowner [id]
-تنظیم سرگروه
-
+Will set id as owner
 !setflood [value]
-تنظیم مقدار حداکثر اسپم
-
+Set [value] as flood sensitivity
 !stats
-وضعیت گروه (ارسال پیام کاربران)
-
+Simple message statistics
 !save [value] <text>
-ذخیره ی مقدار
-
+Save <text> as [value]
 !get [value]
-دریافت مقدار ذخیره شده
-
+Returns text of [value]
 !clean [modlist|rules|about]
-حذف کردن
-
+Will clear [modlist|rules|about] and set it to nil
 !res [username]
-فهمیدن اسکم و ایدی شخص
+returns user id
 "!res @username"
-
 !log
-دریافت لاگ های گروه
-
+will return group logs
 !banlist
-لیست بن شده ها
-
-شما کد ها را هم میتوانید با ! انجام دهید هم با /
-
-
-فقط مدیر گروه میتواند ربات ها را درگروه اد کند
-
-سوال دارید ؟
-@ShopBuy
-
+will return group ban list
+soali darid ? @shopbuy
 ]]
-  }
-  serialize_to_file(config, './data/config.lua')
-  print('saved config into ./data/config.lua')
+}
+serialize_to_file(config, './data/config.lua')
+print('saved config into ./data/config.lua')
 end
-
 function on_our_id (id)
-  our_id = id
+our_id = id
 end
-
 function on_user_update (user, what)
-  --vardump (user)
+--vardump (user)
 end
-
 function on_chat_update (chat, what)
-
 end
-
 function on_secret_chat_update (schat, what)
-  --vardump (schat)
+--vardump (schat)
 end
-
 function on_get_difference_end ()
 end
-
 -- Enable plugins in config.json
 function load_plugins()
-  for k, v in pairs(_config.enabled_plugins) do
-    print("Loading plugin", v)
-
-    local ok, err =  pcall(function()
-      local t = loadfile("plugins/"..v..'.lua')()
-      plugins[v] = t
-    end)
-
-    if not ok then
-      print('\27[31mError loading plugin '..v..'\27[39m')
-      print(tostring(io.popen("lua plugins/"..v..".lua"):read('*all')))
-      print('\27[31m'..err..'\27[39m')
-    end
-
-  end
+for k, v in pairs(_config.enabled_plugins) do
+print("Loading plugin", v)
+local ok, err = pcall(function()
+local t = loadfile("plugins/"..v..'.lua')()
+plugins[v] = t
+end)
+if not ok then
+print('\27[31mError loading plugin '..v..'\27[39m')
+print(tostring(io.popen("lua plugins/"..v..".lua"):read('*all')))
+print('\27[31m'..err..'\27[39m')
 end
-
-
+end
+end
 -- custom add
 function load_data(filename)
-
-	local f = io.open(filename)
-	if not f then
-		return {}
-	end
-	local s = f:read('*all')
-	f:close()
-	local data = JSON.decode(s)
-
-	return data
-
+local f = io.open(filename)
+if not f then
+return {}
 end
-
+local s = f:read('*all')
+f:close()
+local data = JSON.decode(s)
+return data
+end
 function save_data(filename, data)
-
-	local s = JSON.encode(data)
-	local f = io.open(filename, 'w')
-	f:write(s)
-	f:close()
-
+local s = JSON.encode(data)
+local f = io.open(filename, 'w')
+f:write(s)
+f:close()
 end
-
 -- Call and postpone execution for cron plugins
 function cron_plugins()
-
-  for name, plugin in pairs(plugins) do
-    -- Only plugins with cron function
-    if plugin.cron ~= nil then
-      plugin.cron()
-    end
-  end
-
-  -- Called again in 2 mins
-  postpone (cron_plugins, false, 120)
+for name, plugin in pairs(plugins) do
+-- Only plugins with cron function
+if plugin.cron ~= nil then
+plugin.cron()
 end
-
+end
+-- Called again in 2 mins
+postpone (cron_plugins, false, 120)
+end
 -- Start and load values
 our_id = 0
 now = os.time()
